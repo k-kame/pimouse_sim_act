7d0356bf0e17e34041b7804612a57b86ae3af639
k-kame <kentaro.kameyama@gmail.com>
2021-01-16 17:05:16 +0900

    leftと合うようにwallの変数名変更．コメント付加

--- scripts/wall_around.py
+++ scripts/wall_around.py
@@ -13,14 +13,17 @@ from pimouse_ros.msg import LightSensorValues
 
 class WallAround():
     def __init__(self):
+        # 光センサのサブスクライバー
+        rospy.Subscriber('/lightsensors', LightSensorValues, self.sensor_callback)
+        # モータに速度を入力するためのパブリッシャ（ここが違う）
         self.cmd_vel = rospy.Publisher('/cmd_vel',Twist,queue_size=1)
-
+        # 光センサのメッセージオブジェクト
         self.sensor_values = LightSensorValues()
-        rospy.Subscriber('/lightsensors', LightSensorValues, self.callback_lightsensors)
 
-    def callback_lightsensors(self,messages):
+    def sensor_callback(self,messages):
         self.sensor_values = messages
 
+    # メインループの中身と動作のための関数　＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
     def wall_front(self,ls):
         return ls.left_forward > 50 or ls.right_forward > 50
 
@@ -31,14 +34,19 @@ class WallAround():
         return ls.left_side > 50
 
     def run(self):
+        # 更新頻度の設定
         rate = rospy.Rate(20)
+        # node:motors に渡すデータ(Twist型)の定義と初期化（前進速度と旋回速度を0に設定）
         data = Twist()
-
         data.linear.x = 0.0
         data.angular.z = 0.0
-        while not rospy.is_shutdown():
-            data.linear.x = 0.3
 
+        # この while がメインループ
+        while not rospy.is_shutdown():
+            # 速度 0.1 で前進
+            data.linear.x = 0.1
+
+            # node:lightsensors から得たデータ(LightSensorValues型)に基づいて行動分岐
             if self.wall_front(self.sensor_values):
                 data.angular.z = - math.pi
             elif self.too_right(self.sensor_values):
@@ -49,16 +57,25 @@ class WallAround():
                 e = 1.0 * (50 - self.sensor_values.left_side)
                 data.angular.z = e * math.pi / 180.0
                 
+            # node:motors にデータをパブリッシュ cmd_vel
             self.cmd_vel.publish(data)
+            # 次のタイミングまでスリープ
             rate.sleep()
+    # メインループの中身（ここまで）＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
 
 if __name__ == '__main__':
+    # ノード初期化
     rospy.init_node('wall_trace')
 
+    ### サービスの準備
+    # サービスが立ち上がるまでサービスコールをストップ
     rospy.wait_for_service('/motor_on')
     rospy.wait_for_service('/motor_off')
+    # シャットダウンのためのフックを登録
     rospy.on_shutdown(rospy.ServiceProxy('/motor_off',Trigger).call)
+    # /motor_on という名前でTrigger型のサービスを定義
     rospy.ServiceProxy('/motor_on',Trigger).call()
 
+    # run 関数の呼び出し
     w = WallAround()
     w.run()
